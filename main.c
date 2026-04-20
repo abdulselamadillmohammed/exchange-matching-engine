d9f952e9b1aea422fcdf5a370f9563632c165887
Abdulselam Adill <forcoding39@gmail.com>
2026-04-20 14:43:41 -0400

    add: signal handler function signature

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib,h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+
+#include "signal_handlers.h"
+
+int main(int argc, char **argv) {
+
+
+
+
+
+    return EXIT_SUCESS;
+}
---
+++ signal_handers.h
@@ -0,0 +1,10 @@
+#ifndef SIGNAL_HANDLERS_H
+#define SIGNAL_HANDLERS_H
+
+
+// This is meant to block signals
+void block_signals();
+
+#endif
+
+
