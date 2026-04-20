9e03bda06ce69464b0748d2238a5a0b83951fcf6
Abdulselam Adill <forcoding39@gmail.com>
2026-04-20 14:56:34 -0400

    add: first signal handler

---
+++ signal_handlers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+
+void block_signals(int sig) {
+    if (sig == SIGINT)
+        signal(sig, SIG_IGN);
+    else
+        fprintf(stdout, "NOT ctrl + c sigal recived!\n");
+        exit(EXIT_SUCCESS);
+}
+
